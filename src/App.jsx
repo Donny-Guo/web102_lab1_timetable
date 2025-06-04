2a3598020daf4b2888a12641599d022e316a8057
Donny <107092000+Donny-Guo@users.noreply.github.com>
2025-06-04 13:26:12 -0700

    Change heading

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
       <h1>
         Itinerary for 7 Days in Chicago 🏙
       </h1>
-      <h2>
+      <h3>
         Welcome to Chicago, Natalia! Check out this calendar to get to know the city and see all the sights during your stay.
-      </h2>
+      </h3>
       <Calendar />
     </div>
   )
